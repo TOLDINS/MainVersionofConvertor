fc3fb9360712f5e15b581ee13a3cb5b0dc2dd79c
Kollkaa <1kolia1@ukr.net>
2020-02-11 20:59:16 +0200

    edsf

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const exphbs=require('express-handlebars');
 const mongoose=require('mongoose');
 const hbss=exphbs.create(
     {
-        layoutsDir: "views/layouts", 
+        layoutsDir: "views/layouts",
         defaultLayout: "empty",
         extname: "hbs"
     });
 app.engine("hbs", hbss.engine);
 app.set("view engine", "hbs");
 app.set('views','views');
- 
+
 app.use('/',startRoute);
 mongoose.connect('mongodb+srv://user:1234@cluster0-9gu3c.mongodb.net/test?retryWrites=true&w=majority',{
     useUnifiedTopology : true,
     useNewUrlParser : true
 }).then(()=>console.log('Db connected')).catch(()=>console.log(error));
 
-
+//
 
 
 
